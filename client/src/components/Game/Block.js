46ed14a80ed534215be13fff74878aa40915fe73
kikoken <kikoken@gmail.com>
2020-11-14 14:02:03 -0300

    added click selection outside the bomb and game over detection

--- client/src/components/Game/Block.js
+++ client/src/components/Game/Block.js
@@ -2,17 +2,23 @@ import React, { useEffect, useState } from 'react'
 
 const Block = ({content}) => {
     const [item, setItem] = useState('0')
+    const [pointer, setPointer] = useState('')
     
     useEffect(() => {
         setItem(content)
-    }, [item])
+    }, [item, content])
 
     const onClickHandler = (e) => {
-        console.log(item)
+        if(item === '*') {
+            console.log('Game Over!')
+        } else {
+            console.log(item)
+            setPointer('checked')
+        }
     }
 
     return (
-        <div className="block" onClick={onClickHandler}>{item}</div>
+        <div className={`block ${pointer}`} onClick={onClickHandler}>{item}</div>
     )
 }
 
